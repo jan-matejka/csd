d1e4293a4f65c768571c9c0ab0744709d64ebeb9
yac <yac@blesmrt.net>
2017-01-13 20:22:53 +0100

    Resolve the relative urls using uriparser

--- src/csd.cpp
+++ src/csd.cpp
@@ -1,8 +1,12 @@
 #include <fstream>
+#include <iomanip>
 #include <set>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
+
+#include <boost/algorithm/string/predicate.hpp>
 
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
@@ -10,7 +14,104 @@
 
 #include <gumbo.h>
 
+#include <uriparser/Uri.h>
+
 using namespace std;
+
+class UriParseFailure : public runtime_error {
+    public:
+
+    UriParseFailure(int error_code)
+        : runtime_error("uri parsing error code: " + to_string(error_code)) {
+    }
+};
+
+class Uri {
+    // FIXME: pick a sane lib here and package it where needed
+    // sane looking libs:
+    // - https://github.com/datacratic/google-url
+    // - https://github.com/chmike/CxxUrl
+    // FIXME: this implemenation with uriparser probably doesn't support
+    // anything other than most common formats.
+    // Unsupported includes: explicit ports, file access, probably
+    // localized uris
+    public:
+    const string str;
+    Uri(string url) : str(url) {
+        state.uri = &uri;
+
+        if (uriParseUriA(&state, str.c_str()) != URI_SUCCESS) {
+            throw new UriParseFailure(state.errorCode);
+        }
+
+        scheme = _mkstr(uri.scheme);
+        port   = _mkstr(uri.portText);
+        host   = _mkstr(uri.hostText);
+        is_abs = uri.absolutePath;
+        is_prl = boost::starts_with(str, "//");
+
+        uriFreeUriMembersA(&uri);
+    }
+
+    string get_absolute(string base) {
+        return get_absolute(Uri(base));
+    }
+    /**
+     * \param[in] base uri of the page to resolve relative links against.
+     *   eg.: Uri("foo").get_absolute("http://example.com") == "http://example.com/foo"
+     */
+    string get_absolute(const Uri base) const {
+        // TODO: cover by unit tests
+        if (!scheme.empty()) {
+            return str;
+        }
+
+        if (!is_abs) {
+            return base.str + "/" + str;
+        }
+
+        if (is_prl) {
+            // FIXME: scheme-relative-file-URL not implemented.
+            // The path-absolute-non-Windows-file-URL doesn't make sense
+            // to me.
+            return base.scheme + ":" + str;
+        }
+
+        if (host.empty()) {
+            return base.scheme + "://" + base.host + str;
+        }
+
+        throw runtime_error("Failed to absolutize " + str + " with base "
+            + base.str);
+    }
+
+    private:
+    UriParserStateA state;
+    UriUriA uri;
+    string host;
+    string port;
+    string scheme;
+    /**
+     * True if the URL starts with a '/', false otherwise (including
+     * starting with scheme)
+     */
+    bool is_abs;
+
+    /**
+     * True if uri is Protocol Relative Link
+     * https://url.spec.whatwg.org/#syntax-url-scheme-relative
+     */
+    bool is_prl;
+
+    string _mkstr(UriTextRangeA x) const {
+        if (x.first == NULL || x.afterLast == NULL) {
+            return string();
+            // TODO: maybe check if only one of those is NULL?
+        }
+
+        return string(x.first, x.afterLast);
+    }
+};
 
 /**
  * Parses file references from HTML document.
@@ -40,7 +141,7 @@ class Html5Parser {
 
     GumboOutput* output;
     string base_url;
-    vector<string> relative_urls;
+    vector<Uri> relative_urls;
 
     /**
      * Parses raw urls from the given HTML document
@@ -66,7 +167,7 @@ class Html5Parser {
 
         GumboAttribute* href;
         if ((href = gumbo_get_attribute(&node->v.element.attributes, "href"))) {
-            relative_urls.push_back(href->value);
+            relative_urls.push_back(Uri(href->value));
         }
     }
 
@@ -76,7 +177,7 @@ class Html5Parser {
      */
     void munge() {
         for (const auto &x : relative_urls) {
-            urls.insert(x);
+            urls.insert(x.get_absolute(Uri(base_url)));
         }
     }
 };
